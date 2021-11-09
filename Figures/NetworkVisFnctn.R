e798967b7145f4dd88d5c2f48149567ad742fa67
Kayla <kmblincow@gmail.com>
2021-11-09 14:12:29 -0400

    Update NetworkVisFnctn.R

--- Figures/NetworkVisFnctn.R
+++ Figures/NetworkVisFnctn.R
@@ -22,6 +22,11 @@ ccm_plot <- function(infile, siglevel, edgecolor, outfile){
   #load the data file
   load(infile)
   
+  #remove any connections that did not show convergence
+  if(infile == "TCombo_WarmDaily_ccm_webs_SCB3.RData"){
+    ccm_matrix[2,4] <- 0
+  }
+  
   #only pull out significant results from ccm_matrix
   for(i in 1:nrow(ccm_matrix)){
     for(j in 1:ncol(ccm_matrix)){
@@ -30,6 +35,8 @@ ccm_plot <- function(infile, siglevel, edgecolor, outfile){
       }
     }
   }
+  
+  
   
   #convert the ccm_matrix to the right format
   nodes <- as.data.frame(colnames(ccm_matrix))
@@ -47,7 +54,7 @@ ccm_plot <- function(infile, siglevel, edgecolor, outfile){
   
   #populate third column with the rho values from the cross map
   for(i in 1:nrow(edges)){
-    edges[i,3] <- ccm_matrix[edges[i,1], edges[i,2]]
+    edges[i,3] <- ccm_matrix[edges[i,2], edges[i,1]]
   }
   
   
@@ -76,83 +83,19 @@ ccm_plot <- function(infile, siglevel, edgecolor, outfile){
     geom_node_point(size = 15, color = "gray10")+
     geom_node_text(aes(label = label), size = 5, color = "white") +
     labs(edge_width = "rho", edge_alpha = "rho") +
-    theme_graph()
+    theme_graph() +
+    theme(legend.position = "bottom",
+          legend.title = element_text(size = 20),
+          legend.text = element_text(size = 16)) 
   #stupid font warnings...
   
   png(outfile, 
       units="in", 
-      width=8, 
-      height=7, 
+      width=7.5, 
+      height=8, 
       pointsize=12, 
       res=400)
   suppressWarnings(print(p1))
   dev.off()
 }
-
-
-#####TESTING OUT THE FUNCTION#####
-#load the data file
-load("TCombo_CoolDaily_ccm_webs_SCB3.RData")
-
-#only pull out significant results from ccm_matrix
-for(i in 1:nrow(ccm_matrix)){
-  for(j in 1:ncol(ccm_matrix)){
-    if(ccm_significance[i,j] > 0.05){
-      ccm_matrix[i,j] <- 0
-    }
-  }
-}
-
-#convert the ccm_matrix to the right format
-nodes <- as.data.frame(colnames(ccm_matrix))
-colnames(nodes) <- "label"
-
-nodes <- nodes %>% rowid_to_column("id")
-
-nodes <- as.tbl(nodes)
-
-#create a matrix for output
-edges <- matrix(NA, nrow(ccm_matrix)^2, 3)
-
-edges[,1:2] <- rep(1:ncol(ccm_matrix), nrow(ccm_matrix))
-edges[,1] <- sort(edges[,1])
-
-#populate third column with the rho values from the cross map
-for(i in 1:nrow(edges)){
-  edges[i,3] <- ccm_matrix[edges[i,1], edges[i,2]]
-}
-
-
-edges <- as.data.frame(edges)
-colnames(edges) <- c("driver", "response", "weight")
-edges <- edges[edges$weight!=0,]
-
-#set my color scheme for nodes
-mycolor <- c(gray.colors(6))
-
-#plot the results
-ccm_igraph <- graph_from_data_frame(d = edges, vertices = nodes, directed = TRUE)
-ccm_tidy  <- tbl_graph(nodes = nodes, edges = edges, directed = T)
-ccm_igraph_tidy <- as_tbl_graph(ccm_igraph)
-
-
-
-p1 <- ggraph(ccm_igraph, layout = "linear") + 
-  geom_edge_arc(aes(width = weight, alpha = weight), color = "red") + 
-  scale_edge_width(range = c(1, 5), 
-                   breaks = c(0.2, 0.4, 0.6, 0.8, 1),
-                   limits = c(0,1)) +
-  scale_edge_alpha(range = c(0, 1), 
-                   breaks = c(0.2, 0.4, 0.6, 0.8, 1),
-                   limits = c(0,1)) +
-  geom_node_point(size = 15, color = "gray10")+
-  geom_node_text(aes(label = label), size = 5, color = "white") +
-  labs(edge_width = "rho", edge_alpha = "rho") +
-  theme_graph()
-#stupid font warnings...
-
-
-
-
-
 
